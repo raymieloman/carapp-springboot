9d1165054472794dabe34e0059dc83a8cdd0c246
Raymond Loman <r.loman@recognize.nl>
2022-09-27 16:51:20 +0200

    Add localdate with setter

--- src/main/java/com/ilionx/carapp/model/Car.java
+++ src/main/java/com/ilionx/carapp/model/Car.java
@@ -1,9 +1,24 @@
 package com.ilionx.carapp.model;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 public class Car {
+
+    private LocalDate dateOfBuild;
+
+    public LocalDate getDateOfBuild() {
+        return dateOfBuild;
+    }
+
+    public void setDateOfBuild(String datumString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        LocalDate result = LocalDate.parse(datumString, formatter);
+        this.dateOfBuild = result;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
