6eb105c40d3f43361b9748c905dbfc89ae5540ac
Raymond Loman <r.loman@recognize.nl>
2022-09-27 14:01:12 +0200

    Work in progress

--- src/main/java/com/ilionx/carapp/persistence/CarRepository.java
+++ src/main/java/com/ilionx/carapp/persistence/CarRepository.java
@@ -2,10 +2,9 @@ package com.ilionx.carapp.persistence;
 
 import com.ilionx.carapp.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CarRepository extends JpaRepository<Car, Long> {
-
-
+public interface CarRepository extends JpaRepository<Car, Long>, JpaSpecificationExecutor<Car> {
 }
---
+++ src/main/java/com/ilionx/carapp/specs/CarSpecification.java
@@ -0,0 +1,25 @@
+package com.ilionx.carapp.specs;
+
+import com.ilionx.carapp.model.Car;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+public class CarSpecification implements Specification<Car> {
+
+    private int maxMileage;
+
+    public CarSpecification(int maxMileage) {
+        this.maxMileage = maxMileage;
+    }
+
+    @Override
+    public Predicate toPredicate(Root<Car> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+
+        return criteriaBuilder.greaterThanOrEqualTo(root.);
+
+    }
+}
