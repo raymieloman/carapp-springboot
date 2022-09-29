2ad89b38df23e559a3817ce8646103cb5a4167c7
Raymond Loman <r.loman@recognize.nl>
2022-09-29 11:30:14 +0200

    Add unitest for Car

---
+++ src/test/java/com/ilionx/carapp/model/CarTest.java
@@ -0,0 +1,19 @@
+package com.ilionx.carapp.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class CarTest {
+
+    private Car car;
+
+    @Test
+    public void testGetSetBrand() {
+
+        car = new Car();
+        car.setBrand("Mercedes");
+
+        assertEquals("Mercedes", car.getBrand());
+    }
+}
